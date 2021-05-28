400f5bae2e3cbab9143d3503a343df583fbf1b8a
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2021-05-28 22:00:34 +0200

    Add feature "robotsHide"

--- mod_toastghsvs.php
+++ mod_toastghsvs.php
@@ -1,8 +1,16 @@
 <?php
 defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
 use Joomla\CMS\Plugin\PluginHelper;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Helper\ModuleHelper;
+
+if ($params->get('robotsHide', 1) === 1
+	&& Factory::getApplication()->client->robot === true)
+{
+	return;
+}
 
 if ($params->get('prepare_content', 0))
 {
